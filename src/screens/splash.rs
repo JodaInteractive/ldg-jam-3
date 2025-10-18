6997f1b6c8630aa0a613f72b6053d2928d488a15
Joda <JodaInteractive@protonmail.com>
2025-10-18 15:51:19 -0500

    refactor out stars

--- src/screens/splash.rs
+++ src/screens/splash.rs
@@ -3,14 +3,9 @@ use rand::random_range;
 
 use crate::{
     PIXEL_PERFECT_LAYERS,
-    screens::{Screen, game::Star},
+    screens::Screen,
+    stars::{Star, StarPool},
 };
-
-#[derive(Resource)]
-struct StarPool {
-    active_count: usize,
-    inactive_stars: Vec<Entity>,
-}
 
 #[derive(Component)]
 struct Title;
@@ -22,15 +17,7 @@ struct Developer;
 struct FadeOut;
 
 pub(super) fn plugin(app: &mut App) {
-    app.insert_resource::<StarPool>(StarPool {
-        active_count: 0,
-        inactive_stars: Vec::new(),
-    });
     app.add_systems(OnEnter(Screen::Splash), setup_splash);
-    app.add_systems(
-        FixedUpdate,
-        (spawn_stars, update_stars).run_if(in_state(Screen::Splash).or(in_state(Screen::Title))),
-    );
     app.add_systems(
         FixedUpdate,
         (splash_update, update_title, update_developer, fade_out).run_if(in_state(Screen::Splash)),
@@ -108,86 +95,6 @@ fn splash_update(mut state: ResMut<NextState<Screen>>, time: Res<Time>, mut time
 
     if timer.is_finished() {
         state.set(Screen::Title);
-    }
-}
-
-fn spawn_stars(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut star_pool: ResMut<StarPool>,
-) {
-    let x = random_range(-428.0..428.0);
-    let y = random_range(240.0..480.0);
-    let variant = random_range(1..=2);
-
-    let brightness: f32 = random_range(0.6..1.0);
-    let brightness = brightness * brightness;
-
-    let color = if random_range(0.0..1.0) < 0.5 {
-        Color::linear_rgb(brightness, brightness * 0.9, brightness * 0.7) // warmer star
-    } else {
-        Color::linear_rgb(brightness * 0.8, brightness * 0.9, brightness) // cooler star
-    };
-
-    let star_entity = star_pool.inactive_stars.pop();
-
-    if let Some(entity) = star_entity {
-        commands.entity(entity).insert((
-            Transform {
-                translation: Vec3::new(x, y, -10.0),
-                ..default()
-            },
-            Sprite {
-                image: asset_server.load(format!("sprites/stars/star{variant}.png")),
-                color,
-                ..default()
-            },
-            Star {
-                speed: random_range(0.3..=0.6),
-            },
-        ));
-        star_pool.active_count += 1;
-    } else if star_pool.active_count < 600 {
-        spawn_new_star(&mut commands, &asset_server, x, y, variant, color);
-        star_pool.active_count += 1;
-    }
-}
-
-fn spawn_new_star(
-    commands: &mut Commands,
-    asset_server: &Res<AssetServer>,
-    x: f32,
-    y: f32,
-    variant: u8,
-    color: Color,
-) {
-    commands.spawn((
-        Name::new("Star"),
-        Star {
-            speed: random_range(0.3..=0.6),
-        },
-        Sprite {
-            image: asset_server.load(format!("sprites/stars/star{variant}.png")),
-            color,
-            ..default()
-        },
-        Transform {
-            translation: Vec3::new(x, y, -10.0),
-            ..default()
-        },
-        DespawnOnExit(Screen::Game),
-        PIXEL_PERFECT_LAYERS,
-    ));
-}
-
-fn update_stars(mut stars: Query<(&mut Transform, &mut Star)>) {
-    for (mut transform, mut star) in stars.iter_mut() {
-        transform.translation -= Vec3::Y * star.speed;
-        if transform.translation.y < -240.0 {
-            transform.translation.y = 240.0 + random_range(0.0..20.0);
-            transform.translation.x = random_range(-400.0..400.0);
-            star.speed = random_range(0.25..=0.5);
-        }
     }
 }
 
