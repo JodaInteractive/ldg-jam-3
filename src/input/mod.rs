913cedd50f7ff07d626fc82f1d6e14522bd76dc4
Joda <JodaInteractive@protonmail.com>
2025-10-19 04:51:43 -0500

    add gamepad support

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -25,17 +25,36 @@ fn setup_input(mut commands: Commands) {
     use leafwing_input_manager::prelude::*;
 
     let mut player_input_map = InputMap::default();
+    player_input_map.insert(Action::Up, GamepadControlDirection::LEFT_UP.threshold(0.25));
+    player_input_map.insert(
+        Action::Down,
+        GamepadControlDirection::LEFT_DOWN.threshold(0.25),
+    );
+    player_input_map.insert(
+        Action::Left,
+        GamepadControlDirection::LEFT_LEFT.threshold(0.25),
+    );
+    player_input_map.insert(
+        Action::Right,
+        GamepadControlDirection::LEFT_RIGHT.threshold(0.25),
+    );
     player_input_map.insert(Action::Up, KeyCode::KeyW);
     player_input_map.insert(Action::Up, KeyCode::ArrowUp);
+    player_input_map.insert(Action::Up, GamepadButton::DPadUp);
     player_input_map.insert(Action::Down, KeyCode::KeyS);
     player_input_map.insert(Action::Down, KeyCode::ArrowDown);
+    player_input_map.insert(Action::Down, GamepadButton::DPadDown);
     player_input_map.insert(Action::Left, KeyCode::KeyA);
     player_input_map.insert(Action::Left, KeyCode::ArrowLeft);
+    player_input_map.insert(Action::Left, GamepadButton::DPadLeft);
     player_input_map.insert(Action::Right, KeyCode::KeyD);
     player_input_map.insert(Action::Right, KeyCode::ArrowRight);
+    player_input_map.insert(Action::Right, GamepadButton::DPadRight);
     player_input_map.insert(Action::Select, KeyCode::Space);
     player_input_map.insert(Action::Shoot, KeyCode::Space);
+    player_input_map.insert(Action::Shoot, GamepadButton::South);
     player_input_map.insert(Action::Select, KeyCode::Enter);
+    player_input_map.insert(Action::Select, GamepadButton::South);
     player_input_map.insert(Action::Pause, KeyCode::Escape);
 
     commands.spawn(player_input_map);
--- src/sundry.rs
+++ src/sundry.rs
@@ -2,5 +2,7 @@ use bevy::prelude::Color;
 
 pub const WHITE: Color = Color::srgb_u8(235, 237, 233);
 // pub const TRANSPARENT_WHITE: Color = Color::srgba_u8(235, 237, 233, 0);
-pub const LIGHT_GRAY: Color = Color::srgb_u8(199, 207, 204);
-pub const TRANSPARENT_LIGHT_GRAY: Color = Color::srgba_u8(199, 207, 204, 0);
+// pub const LIGHT_GRAY: Color = Color::srgb_u8(199, 207, 204);
+// pub const TRANSPARENT_LIGHT_GRAY: Color = Color::srgba_u8(199, 207, 204, 0);
+pub const MEDIUM_GRAY: Color = Color::srgb_u8(168, 181, 178);
+pub const TRANSPARENT_MEDIUM_GRAY: Color = Color::srgba_u8(168, 181, 178, 0);
